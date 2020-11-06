a82cdc57d1f587817855f6160b24288fde28b128
evanokeeffe <evanokeeffe@pop-os.localdomain>
2020-11-06 14:31:11 +0000

    adding all files

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errexit
+
+#echo $@
+#echo $0
+#echo $1
+#echo " "
+
+case "$1" in
+    sh|bash)
+        set -- "$@"
+    ;;
+    *.py)
+        set -- python3 "$@"
+    ;;
+    *)
+        set -- "$@"
+    ;;
+esac
+
+exec "$@"
+
